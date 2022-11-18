db7848c3ee6dfd0db9bf53c9febd78c4e61b0d5d
Madhavi210 <jmadhavi156@gmail.com>
2022-11-18 18:29:01 +0530

    u

--- app.js
+++ app.js
@@ -42,3 +42,17 @@ function convertDateToString(dateEnteredByUser) {
     return strDate;
 
 }
+
+function dateFormates(dateEnteredByUser) {
+    var dateReceived = convertDateToString(dateEnteredByUser);
+
+    // dateFormates
+    const DDMMYYYY = dateReceived.day + dateReceived.month + dateReceived.year;
+    const MMDDYYYY = dateReceived.month + dateReceived.day + dateReceived.year;
+    const YYYYMMDD = dateReceived.year + dateReceived.month + dateReceived.day;
+    const DDMMYY = dateReceived.day + dateReceived.month + dateReceived.year.slice(-2);
+    const MMDDYY = dateReceived.month + dateReceived.day + dateReceived.year.slice(-2);
+    const YYMMDD = dateReceived.year.slice(-2) + dateReceived.month + dateReceived.day;
+    const formatsOfDate = [DDMMYYYY, MMDDYYYY, YYYYMMDD, DDMMYY, MMDDYY, YYMMDD];
+    return formatsOfDate;
+}
