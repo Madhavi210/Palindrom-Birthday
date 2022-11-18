d620fae4c0bac99ba9dabd0331f1d7af964731d7
Madhavi210 <jmadhavi156@gmail.com>
2022-11-18 18:27:00 +0530

    u

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var inputDate = document.querySelector(".input-date");
 var checkBtn = document.querySelector(".check-btn");
 var outputText = document.querySelector(".result");
+
+function reverseStr(str) {
+    var listOfChars = str.split("");
+    var reversedListOfChar = listOfChars.reverse();
+    var reversedString = reversedListOfChar.join("");
+    return reversedString;
+}
